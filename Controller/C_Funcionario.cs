f923e8caa377bb6fc00c20b3275269733fe2e0a1
João Caetano <jcgam3r69@gmail.com>
2023-11-01 22:42:01 -0300

    criando tela de cadastro de logins

--- Controller/C_Funcionario.cs
+++ Controller/C_Funcionario.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Loja_Unifunec.Conection;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +12,10 @@ namespace Loja_Unifunec.Controller
     public static class C_Funcionario
     {
 
-
+        static Conexao conection = new Conexao();
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static DataTable dtUf;
 
     }
 }
